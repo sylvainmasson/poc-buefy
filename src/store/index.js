5a6778ff44b4e5695a290285d935f58223a01a6f
sylvainmasson <masson_sylvain@yahoo.fr>
2020-04-20 14:07:13 +0200

    Ajout vuex-persist local storage

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,26 @@ import Vuex from 'vuex'
 import * as user from '@/store/modules/user'
 import * as pagination from '@/store/modules/pagination'
 import * as alert from '@/store/modules/alert'
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
+
+/**
+ * Mise en place du stockage local pour vuex dans host-vuex
+ */
+const vuexLocal = new VuexPersistence({
+  key: window.location.host + '-vuex',
+  storage: window.localStorage
+})
 
 export default new Vuex.Store({
   modules: {
     user,
     pagination,
     alert
-  }
+  },
+  mutations: {
+    RESTORE_MUTATION: vuexLocal.RESTORE_MUTATION // this mutation **MUST** be named "RESTORE_MUTATION"
+  },
+  plugins: [vuexLocal.plugin]
 })
